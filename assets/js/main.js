006e6183b6d117e31af11553ecb474ced0773f50
karfaisarra <karfaisarra@gmail.com>
2022-10-10 17:02:28 +0200

    fix commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,31 +5,36 @@ containerEl = document.querySelector('.container');
 // stampi in console i numeri da 1 a 100
 for (let i = 1; i <= 100; i++) {
     //Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”
-    let elements = `<li class="box_${i}">${i}</li>`;
+    let elements = document.createElement('p');
     let fizz;
     let buzz;
     let fizzbuzz;
     if ((i % 3 == 0) && (i % 5 == 0)) { 
         console.log("FizzBuzz");
-        fizzbuzz = 'FizzBuzz';
-        containerEl.append(fizzbuzz);
+        elements.innerHTML +='FizzBuzz'
+        containerEl.append(elements);
+        elements.style.backgroundColor = "#DE5471";
+
     } 
     //per i multipli di 3 stampi “Fizz” al posto del numero   
     else if (i % 3 == 0){
         console.log("Fizz");
-        fizz = 'Fizz';
-        containerEl.append(fizz);
+        elements.innerHTML += 'Fizz'
+        containerEl.append(elements);
+        elements.style.backgroundColor = "#61D2A4";
     }  
     //per i multipli di 5 stampi “Buzz”  
     else if (i % 5 == 0){ 
         console.log("Buzz");
-        buzz = 'Buzz';
-        containerEl.append(buzz);
-
+        elements.innerHTML += 'Buzz'
+        containerEl.append(elements);
+        elements.style.backgroundColor = "#F9D277";
     }    
     else {
         console.log(i);
-        containerEl.append(`${i}`);
+        elements.innerHTML +=`${i}`
+        containerEl.append(elements);
+        elements.style.backgroundColor = "#3D88AE";
     }
     
 }
