e0154e35af1b8fe44fe062078c8a8934c30dd105
karfaisarra <karfaisarra@gmail.com>
2022-10-10 15:08:11 +0200

    add commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,16 @@
 // stampi in console i numeri da 1 a 100
 for (let i = 1; i <= 100; i++) {
-    //per i multipli di 3 stampi “Fizz” al posto del numero       
-    if (i % 3 == 0) { 
-        console.log("Fizz");
-    } 
-    //per i multipli di 5 stampi “Buzz” 
-    else if (i % 5 == 0) {
-        console.log("Buzz");
-    } 
-    //Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”  
-    else if ((i % 3 == 0) && (i % 5 == 0)){ 
+    //Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”
+    if ((i % 3 == 0) && (i % 5 == 0)) { 
         console.log("FizzBuzz");
+    } 
+    //per i multipli di 3 stampi “Fizz” al posto del numero   
+    else if (i % 3 == 0){
+        console.log("Fizz");
+    }  
+    //per i multipli di 5 stampi “Buzz”  
+    else if (i % 5 == 0){ 
+        console.log("Buzz");
     }    
     else {
         console.log(i);
