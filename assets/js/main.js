02c76f2246b95319ccee555e17e5488372bcda28
karfaisarra <karfaisarra@gmail.com>
2022-10-11 10:27:05 +0200

    change elements commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,37 +1,33 @@
-let containerEl;
-containerEl = document.querySelector('.container');
+const containerEl = document.querySelector('.container');
 //console.log(containerEl);
 
 // stampi in console i numeri da 1 a 100
 for (let i = 1; i <= 100; i++) {
     //Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”
-    let elements = document.createElement('p');
+    const elements = document.createElement('div');
+    elements.classList.add('box');
     if ((i % 3 == 0) && (i % 5 == 0)) { 
-        console.log("FizzBuzz");
-        elements.innerHTML +='FizzBuzz'
-        containerEl.append(elements);
+        //console.log("FizzBuzz");
+        elements.append('FizzBuzz');
         elements.style.backgroundColor = "#DE5471";
 
     } 
     //per i multipli di 3 stampi “Fizz” al posto del numero   
     else if (i % 3 == 0){
-        console.log("Fizz");
-        elements.innerHTML += 'Fizz'
-        containerEl.append(elements);
+        //console.log("Fizz");
+        elements.append('Fizz');
         elements.style.backgroundColor = "#61D2A4";
     }  
     //per i multipli di 5 stampi “Buzz”  
     else if (i % 5 == 0){ 
-        console.log("Buzz");
-        elements.innerHTML += 'Buzz'
-        containerEl.append(elements);
+        //console.log("Buzz");
+        elements.append('Buzz');
         elements.style.backgroundColor = "#F9D277";
     }    
     else {
-        console.log(i);
-        elements.innerHTML +=`${i}`
-        containerEl.append(elements);
+        //console.log(i);
+        elements.append(i);
         elements.style.backgroundColor = "#3D88AE";
     }
-    
+    containerEl.append(elements);
 }
